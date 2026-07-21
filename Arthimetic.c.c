e67d4a0dbf1b1847512e69baafc4ea883b0ce5e5
Kannika <k98788846@gmail.com>
2026-07-21 22:10:44 +0530

    Add files via upload

---
+++ Arthimetic.c.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  int difference =n1-n2;
+  int product=n1*n2;
+  float quotient=n1/n2;
+  printf("sum is %d",sum);
+  printf("/n");
+  printf("difference is %d",difference);
+  printf("/n");
+  printf("product is %d",product);
+  printf("/n");
+  printf("quotient is %f",quotient);
+
+    return 0;
+}
