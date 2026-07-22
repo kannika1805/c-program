22e883e307e29c21008df2c5017b2f2f5ffb939c
Kannika <k98788846@gmail.com>
2026-07-22 21:51:05 +0530

    Create swapWithTemp.c

---
+++ swapWithTemp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+ int num1=1;
+ int num2=2;
+ int temp;
+ printf("Before swap the num1 and num2 is %d,%d",num1,num2);
+ temp=num1;
+ num1=num2;
+ num2=temp;
+ printf("\nAfter swap num1 and num2 is %d, %d",num1,num2);
+
+}
