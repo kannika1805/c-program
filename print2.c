1ae1cf1297883148743239708989680651ac24fd
Kannika <k98788846@gmail.com>
2026-07-22 21:46:40 +0530

    Create print2.c

---
+++ print2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int age;
+    char name[10];
+    printf("enter your age:");
+    scanf("%d",&age);
+    printf("enter your name:");
+    scanf("%s",name);
+    printf("%d",age);
+    printf("\n%s",name);
+}
